dfefb911def1af49bc6029e0bf3eacbfe25604f5
Nitin308 <114205043+Nitin308@users.noreply.github.com>
2022-11-23 13:00:28 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack and Queue Problem");
+
+            LinkedList objList = new LinkedList();
+            objList.Push(56);
+            objList.Push(30);
+            objList.Push(70);
+            objList.Display();
+        }
+    }
+}
